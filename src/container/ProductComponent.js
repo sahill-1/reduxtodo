a86c9245e5ebb40a3fd94e7588c6b162053dd93c
sahill-1 <sv260397@gmail.com>
2023-08-11 09:47:17 +0530

    product actions

--- src/container/ProductComponent.js
+++ src/container/ProductComponent.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
-    const {id, price, image, category, title} = product;
+    const { id, price, image, category, title } = product;
     return (
-      <div className='four column wide' key={id} style={{width:"30%",margin:"auto", display:"flex", flex:"wrap",  gap:"20px"}}>
-        <div style={{width:"100%", margin:"auto"}}>
-          <img src={image} alt={title} style={{ width:"100%", height:"250px"}}/>
-          <h4>{title}</h4>
-          <p>{price}</p>
-          <h5>{category}</h5>
-        </div>
-    </div>
-    )
-  })
-  return <>
-    {renderList}
-    </>
-}
+      <div
+        className="four column wide"
+        key={id}
+        style={{
+          width: "30%",
+          margin: "auto",
+          display: "flex",
+          flex: "wrap",
+          gap: "20px",
+        }}
+      >
+        <Link to={`/product/${id}`}>
+          {" "}
+          <div style={{ width: "100%", margin: "auto" }}>
+            <img
+              src={image}
+              alt={title}
+              style={{ width: "100%", height: "250px" }}
+            />
+            <h4>{title}</h4>
+            <p>{price}</p>
+            <h5>{category}</h5>
+          </div>
+        </Link>
+      </div>
+    );
+  });
+  return <>{renderList}</>;
+};
 
-export default ProductComponent
+export default ProductComponent;
--- src/container/ProductDetails.js
+++ src/container/ProductDetails.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {axios} from "axios";
+import {useParams} from "react-router-dom";
 
 const ProductDetails = () => {
+  const {productId }= useParams() 
+  
+  const fetchProductDetails = () => {
+    const response = await axios.get("https://fakestoreapi.com/products/")
+  }
   return (
     <div>
+      
+         
       
     </div>
   )
